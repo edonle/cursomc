a5cc18ce7e207cd1a1c5045b1bc20a23a2c4ac2f
Edson Carlos <edonle@gmail.com>
2019-02-07 15:56:33 -0200

    Cliente, TipoCliente, telefones e enderecos.

---
+++ src/main/java/com/prodesp/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.prodesp.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.prodesp.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/com/prodesp/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.prodesp.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.prodesp.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
