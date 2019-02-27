9efb50fc501face3686478a6419f642632c4cefb
Edson Carlos <edonle@gmail.com>
2019-02-27 17:00:02 -0300

    Inserindo Pedido

--- src/main/java/com/prodesp/cursomc/domain/Pagamento.java
+++ src/main/java/com/prodesp/cursomc/domain/Pagamento.java
@@ -10,11 +10,13 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.prodesp.cursomc.domain.enums.EstadoPagamento;
 
 // TODO Estudar
 @Entity
 @Inheritance(strategy=javax.persistence.InheritanceType.JOINED)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 public abstract class Pagamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
